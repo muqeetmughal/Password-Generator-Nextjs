3ac39bb4ac1e0905a3518b5056c2d05d9c31ccf6
muqeetmughal <muqeetmughal786@gmail.com>
2022-11-10 02:36:03 +0500

    content errors fixed

--- components/Content.tsx
+++ components/Content.tsx
@@ -15,12 +15,12 @@ const Content = () => {
                     <h3><b>Strong passwords are unique and random.</b></h3>
 
                     <p>
-                        People aren't very good at coming up with passwords that are either of these things, let alone both. That's why we created <b>"Free Password Generator"</b> Strong Password Generator to create safe, memorable passwords for you. 81% of data breaches are caused by reused or weak passwords, so random, unique passwords are your best defense against online threats.
+                        People aren&apos;t very good at coming up with passwords that are either of these things, let alone both. That&apos;s why we created <b>&quot;Free Password Generator&quot;</b> Strong Password Generator to create safe, memorable passwords for you. 81% of data breaches are caused by reused or weak passwords, so random, unique passwords are your best defense against online threats.
                     </p>
                     <h2 className="card-title">Why should my password be unique?</h2>
 
                     <p>
-                        If you use the same password for both your email account and your bank account login, an attacker only needs to steal one password to gain access to both accounts, doubling your risk. If you've used the same password for 14 different accounts, you're making it very, very easy for an attacker. You can protect yourself by using a generator to create unique passwords that are easy to remember.
+                        If you use the same password for both your email account and your bank account login, an attacker only needs to steal one password to gain access to both accounts, doubling your risk. If you&apos;ve used the same password for 14 different accounts, you&apos;re making it very, very easy for an attacker. You can protect yourself by using a generator to create unique passwords that are easy to remember.
                     </p>
                     <h2 className="card-title">Why should my password be random?</h2>
 
